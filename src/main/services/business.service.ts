3693e45e844e0e323ccade3dcb69ad8930320e73
Gonzalo <thanh.nguyenduc@setel.com>
2024-09-09 22:58:10 +0700

    add

--- src/main/services/business.service.ts
+++ src/main/services/business.service.ts
@@ -6,8 +6,9 @@ export async function getListBusiness(params: {
   const offset = (params.page - 1) * params.perPage;
 
   const countSql = `
-    SELECT COUNT(*) as totalItems FROM BUSINESS_PLAN_DETAILS
-    WHERE ERP_CODE LIKE ?;`;
+    SELECT COUNT(*) as totalItems FROM BUSINESS_PLAN BP
+    JOIN CUSTOMER C ON BP.TAX_CODE = C.TAX_CODE
+    WHERE C.CUSTOMER_NAME LIKE ?;`;
 
   const totalResult = await window.electron.queryDatabase(countSql, [
     `%${params.searchQuery || ''}%`,
@@ -16,11 +17,16 @@ export async function getListBusiness(params: {
   const totalItems = totalResult[0].totalItems;
   const totalPages = Math.ceil(totalItems / params.perPage);
   const result = await window.electron.queryDatabase(
-    `SELECT * FROM BUSINESS_PLAN_DETAILS WHERE ERP_CODE LIKE ?
-    ORDER BY ERP_CODE ASC
+    `SELECT BP.*, C.CUSTOMER_NAME, SUM(CAST(REPLACE(BPD.SELLING_PRICE, ',', '') AS REAL))  AS TOTAL_SELLING_PRICE
+    FROM BUSINESS_PLAN BP
+    JOIN CUSTOMER C ON BP.TAX_CODE = C.TAX_CODE
+    LEFT JOIN BUSINESS_PLAN_DETAILS BPD ON BP.BUSINESS_PLAN_ID = BPD.BUSINESS_PLAN_ID
+    WHERE C.CUSTOMER_NAME LIKE ?
+    GROUP BY BP.BUSINESS_PLAN_ID
+    ORDER BY BP.TAX_CODE ASC
     LIMIT ? OFFSET ?`,
     [
-      `%${params.searchQuery || ''}%`, // For ERP_CODE
+      `%${params.searchQuery || ''}%`, // For CUSTOMER_NAME
       params.perPage,
       offset,
     ],
